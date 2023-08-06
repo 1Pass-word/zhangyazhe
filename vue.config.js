733e56102fcec086a7423679c638fd0cd0298b29
张亚哲 <2676216385@qq.com>
2023-08-06 21:09:07 +0800

    更

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/zhangyazhe/src/views/XiangMu.vue',
+  publicPath: '/zhangyazhe/',
   transpileDependencies: true
 })
